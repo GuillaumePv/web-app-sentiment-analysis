4af16268165df06d8d456bd0fa988abada366b3f
Guillaume Pavé <guillaumepave@gmail.com>
2021-02-25 13:11:30 +0100

    add venv

--- redditScrap.py
+++ redditScrap.py
@@ -7,30 +7,31 @@ api = PushshiftAPI()
 start_epoch=int(dt.datetime(2021, 2, 1).timestamp())
 
 submissions = api.search_submissions(after=start_epoch,
-                            subreddit='wallstreetbets',
+                            subreddit='CryptoCurrency',
                             filter=['url','author', 'title', 'subreddit'],
-                            limit=50)
+                            limit=100)
 
 with open('db/stocks.json') as json_file:
     stocks = json.load(json_file)
 
 data = {}
 for sub in submissions:
-   
+
     words = sub.title.split()
     cashtags = list(set(filter(lambda word: word.lower().startswith('$'),words)))
+    print(sub)
+    print(cashtags)
+#     if len(cashtags) > 0:
+#         for cashtag in cashtags:
+#             if cashtag.split("$")[1] in stocks.keys():
+#                 print(f'found one stock {cashtag.split("$")[1]}')
+#                 data[cashtag.split("$")[1]] = {
+#                     'title': sub.title,
+#                     'author': sub.author,
+#                     'date': dt.datetime.utcfromtimestamp(sub.created_utc).strftime('%Y-%m-%d'),
+#                     'url': sub.url
+#                 }
 
-    if len(cashtags) > 0:
-        for cashtag in cashtags:
-            if cashtag.split("$")[1] in stocks.keys():
-                print(f'found one stock {cashtag.split("$")[1]}')
-                data[cashtag.split("$")[1]] = {
-                    'title': sub.title,
-                    'author': sub.author,
-                    'date': dt.datetime.utcfromtimestamp(sub.created_utc).strftime('%Y-%m-%d'),
-                    'url': sub.url
-                }
-
-with open('db/reddit/wsb/wsb.json', 'w',encoding='utf-8') as outfile:
-    json.dump(data, outfile,indent=4,ensure_ascii=False)
+# with open('db/reddit/wsb/wsb.json', 'w',encoding='utf-8') as outfile:
+#     json.dump(data, outfile,indent=4,ensure_ascii=False)
 
